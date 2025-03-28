4b7098cf08c576e1d25bc150e73d97a86ccd90ef
Coder2 <83895170+Jack253-png@users.noreply.github.com>
2025-03-29 06:27:33 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,14 @@
 set_languages("c++17")
 
---includes("extlibs/freetype.lua")
+includes("extlibs/freetype.lua")
 --includes("extlibs/xxhash.lua")
---includes("extlibs/openal.lua")
+includes("extlibs/openal.lua")
 --includes("extlibs/meshoptimizer.lua")
 --includes("extlibs/yoga.lua")
 --includes("extlibs/stb.lua")
-includes("extlibs/shaderc.lua")
+if not is_plat("bsd") then
+    includes("extlibs/shaderc.lua")
+end
 
 if not is_plat("iphoneos", "harmony", "android") then
     if not is_plat("linux") or is_arch("x86_64", "x64") then 
