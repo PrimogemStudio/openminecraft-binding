358da86190860d6ed76e381b029d5ad2991ddbe0
Jack253-png <guoxiuchen20170402@163.com>
2025-03-29 10:32:01 +0800

    libxau 修改

--- extlibs/glfw.lua
+++ extlibs/glfw.lua
@@ -1,12 +1,8 @@
 package("libxau")
-
     set_homepage("https://www.x.org/")
     set_description("X.Org: A Sample Authorization Protocol for X")
 
-    set_urls("https://www.x.org/archive/individual/lib/libXau-$(version).tar.gz0")
-    add_versions("1.0.10", "51a54da42475d4572a0b59979ec107c27dacf6c687c2b7b04e5cf989a7c7e60c")
-    add_versions("1.0.11", "3a321aaceb803577a4776a5efe78836eb095a9e44bbc7a465d29463e1a14f189")
-    add_versions("1.0.12", "2402dd938da4d0a332349ab3d3586606175e19cb32cb9fe013c19f1dc922dcee")
+    set_sourcedir(path.join(os.scriptdir(), "libxau"))
 
     on_install("macosx", "linux", "bsd", "cross", function (package)
         local configs = {"--sysconfdir=" .. package:installdir("etc"),
@@ -18,6 +14,8 @@ package("libxau")
         if package:config("pic") then
             table.insert(configs, "--with-pic")
         end
+        local pth = path.join(os.scriptdir(), "libxau/configure")
+        io.replace(pth, ".libs", "libs", { plain = true })
         import("package.tools.autoconf").install(package, configs)
     end)
 
